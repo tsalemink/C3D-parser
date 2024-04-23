391c4efca14ff4090816860fc3a81032929c6cb5
tsalemink <tim.nicolas@outlook.com>
2024-04-23 19:06:36 +1200

    Implement `de_identify_c3d` function

--- c3d_parser.py
+++ c3d_parser.py
@@ -1,41 +1,24 @@
 
-import c3d
-import numpy as np
+import os
+
+from c3d_patch import c3d
 
 
-# TODO: Simplify this function.
-def read_c3d(file_path):
+def de_identify_c3d(file_path, output_directory):
+    input_directory, file_name = os.path.split(os.path.abspath(file_path))
+    output_directory = os.path.abspath(output_directory)
+
+    # Currently we prevent overwriting the input file.
+    if input_directory == output_directory:
+        raise IOError("Cannot overwrite input file.")
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
     with open(file_path, 'rb') as handle:
         reader = c3d.Reader(handle)
+        writer = reader.to_writer('copy')
+        writer.get('SUBJECTS').set_str('NAMES', '', 'Subject', 7)
 
-        # Get units.
-        units = reader.get('POINT').get('UNITS').string_value
-
-        # Get marker labels.
-        markers = {'time': []}
-        count = 0
-        marker_labels = ['time']
-        for pl in reader.point_labels:
-            lb = pl.strip()
-            if len(lb) == 0:
-                lb = 'M{:03d}'.format(count)
-                count += 1
-            marker_labels.append(lb)
-            markers[lb] = []
-
-        # Get marker data and analog data.
-        analogs = []
-        for i, points, analog in reader.read_frames():
-            analogs.append(analog[:, 0])
-            markers[marker_labels[0]].append((i-1)*(1/reader.point_rate))
-            for j in range(1, len(marker_labels)):
-                errors = points[j-1, 3:]
-                p = points[j-1, 0:3]
-                for e in errors:
-                    if e == -1:
-                        p = np.asarray([np.NaN, np.NaN, np.NaN])
-                        break
-                markers[marker_labels[j]].append(p)
-
-        c3d_data = {'markers': markers, 'mocap': {"rates": reader.point_rate, "units": units, "header": reader.header}, "analog": analogs}
-    return c3d_data
+    with open(os.path.join(output_directory, file_name), 'wb') as handle:
+        writer.write(handle)
