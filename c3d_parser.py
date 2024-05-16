871235224bd5c0ab751b9f16ea56c2965bd00ec3
tsalemink <tim.nicolas@outlook.com>
2024-05-16 22:11:17 +1200

    Change `harmonise_markers` to work on TRC data

--- c3d_parser.py
+++ c3d_parser.py
@@ -1,7 +1,6 @@
 
 import os
 import json
-import numpy
 
 from trc import TRCData
 
@@ -19,6 +18,15 @@ def parse_c3d(c3d_file, output_directory):
     # Extract TRC data from C3D file.
     trc_data = TRCData()
     trc_data.import_from(c3d_file)
+
+    # Harmonise TRC data.
+    map_file = os.path.join(marker_maps_dir, f"{gait_lab}.json")
+    with open(map_file, 'r') as file:
+        marker_map = json.load(file)
+    markers, frames = harmonise_markers(trc_data, marker_map)
+
+    # Write harmonised TRC data.
+    set_marker_data(trc_data, markers, frames)
     trc_directory = os.path.join(output_directory, 'trc')
     if not os.path.exists(trc_directory):
         os.makedirs(trc_directory)
@@ -30,29 +38,6 @@ def parse_c3d(c3d_file, output_directory):
     if not os.path.exists(de_identified_directory):
         os.makedirs(de_identified_directory)
     de_identify_c3d(c3d_file, de_identified_directory)
-
-    # Initialise C3D data for harmonisation.
-    with open(c3d_file, 'rb') as handle:
-        reader = c3d.Reader(handle)
-        writer = reader.to_writer('copy_metadata')
-
-        frames = []
-        for i, points, analog in reader.read_frames():
-            frames.append((points, analog))
-
-    # Harmonise C3D data.
-    map_file = os.path.join(marker_maps_dir, f"{gait_lab}.json")
-    with open(map_file, 'r') as file:
-        marker_map = json.load(file)
-    harmonise_markers(writer, marker_map, frames)
-    writer.add_frames(frames)
-
-    # Write the harmonised C3D file.
-    harmonised_directory = os.path.join(output_directory, 'harmonised')
-    if not os.path.exists(harmonised_directory):
-        os.makedirs(harmonised_directory)
-    with open(os.path.join(harmonised_directory, c3d_file_name), 'wb') as handle:
-        writer.write(handle)
 
 
 def de_identify_c3d(file_path, output_directory):
@@ -75,34 +60,35 @@ def de_identify_c3d(file_path, output_directory):
         writer.write(handle)
 
 
-def harmonise_markers(c3d_writer, marker_mapping, frames):
+def harmonise_markers(trc_data, marker_mapping):
     """
-    Harmonises the marker set of a `c3d` `Writer` object according to a mapping of markers.
+    Harmonises the marker set of a `TRCData` object according to a mapping of markers. Returns the
+    harmonised marker-set and frame data.
 
-    param c3d_writer: The `c3d` `Writer` object to be updated.
+    param c3d_writer: The `TRCData` object to be updated.
     param marker_mapping: A dictionary mapping the trial marker set to the harmonised marker set.
     """
     # Harmonise marker labels.
-    point_group = c3d_writer.get('POINT')
-    harmonised_labels = []
-    for param in ['LABELS', 'LABELS2']:
-        if param in point_group.param_keys():
-            marker_labels = point_group.get(param).string_array
-            reversed_mapping = {value: key for key, value in marker_mapping.items() if value is not None}
-            for index, value in numpy.ndenumerate(marker_labels):
-                value = value.strip()
-                if value in reversed_mapping:
-                    harmonised_labels.append(reversed_mapping[value])
-                else:
-                    harmonised_labels.append(None)
-            point_group.remove_param(param)
-    c3d_writer.set_point_labels([value for value in harmonised_labels if value])
+    reversed_mapping = {value: key for key, value in marker_mapping.items() if value is not None}
+    harmonised_markers = [reversed_mapping[marker] if marker in reversed_mapping else None
+                          for marker in trc_data['Markers']]
 
-    # Remove all non-harmonised data points.
-    for i in range(len(frames)):
-        points, analog = frames[i]
+    # Filter out non-harmonised data points.
+    harmonised_frames = {}
+    for frame_number in trc_data['Frame#']:
+        time, frame = trc_data[frame_number]
         harmonised_points = []
-        for j in range(len(points)):
-            if harmonised_labels[j]:
-                harmonised_points.append(points[j])
-        frames[i] = numpy.array(harmonised_points), analog
+        for i in range(len(frame)):
+            if harmonised_markers[i]:
+                coordinates = frame[i]
+                harmonised_points.append(coordinates)
+        harmonised_frames[frame_number] = [time, harmonised_points]
+    harmonised_markers = list(filter(None, harmonised_markers))
+
+    return harmonised_markers, harmonised_frames
+
+
+def set_marker_data(trc_data, markers, frames):
+    trc_data['Markers'] = markers
+    for frame_number in trc_data['Frame#']:
+        trc_data[frame_number] = frames[frame_number]
