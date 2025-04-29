4e20cf3a627b7a320dfc5d1049f2c61fdd914c25
tsalemink <tim.nicolas@outlook.com>
2025-04-29 13:29:17 +1200

    Additional validation of custom marker sets

--- src/c3d_parser/view/dialogs/marker_set_dialog.py
+++ src/c3d_parser/view/dialogs/marker_set_dialog.py
@@ -1,11 +1,13 @@
 
 import os
+import re
 import json
 
 from PySide6 import QtWidgets
 
 from c3d_parser.core.c3d_parser import marker_maps_dir
 from c3d_parser.view.dialogs.marker_set_import_dialog import MarkerSetImportDialog
+from c3d_parser.settings.general import DEFAULT_STYLE_SHEET, INVALID_STYLE_SHEET
 from c3d_parser.view.ui.ui_marker_set_dialog import Ui_MarkerSetDialog
 
 
@@ -20,9 +22,13 @@ class MarkerSetDialog(QtWidgets.QDialog):
         self._ui = Ui_MarkerSetDialog()
         self._ui.setupUi(self)
 
+        self._marker_mapping = {}
+
         self._make_connections()
+        self._validate_name()
 
     def _make_connections(self):
+        self._ui.lineEditName.textChanged.connect(self._validate_name)
         self._ui.pushButtonImport.clicked.connect(self._import_marker_map)
         self._ui.pushButtonSave.clicked.connect(self._validate_marker_set)
 
@@ -30,14 +36,8 @@ class MarkerSetDialog(QtWidgets.QDialog):
         file_name = f"{self._ui.lineEditName.text()}.json"
         file_path = os.path.join(marker_maps_dir, file_name)
 
-        marker_mapping = {}
-        for marker in markers:
-            combo_box = getattr(self._ui, f"comboBox{marker}")
-            value = combo_box.currentText()
-            marker_mapping[marker] = value if value else None
-
         with open(file_path, 'w') as file:
-            json.dump(marker_mapping, file, indent='\t')
+            json.dump(self._marker_mapping, file, indent='\t')
 
     def load(self, file_path):
         with open(file_path, 'r') as file:
@@ -52,11 +52,43 @@ class MarkerSetDialog(QtWidgets.QDialog):
         if dlg.exec():
             self.load(dlg.get_file_path())
 
+    def _validate_name(self):
+        name = self._ui.lineEditName.text()
+        invalid = not name or re.search(r'[<>:"/\\|?*]', name)
+
+        if invalid:
+            self._ui.lineEditName.setStyleSheet(INVALID_STYLE_SHEET)
+            self._ui.pushButtonSave.setEnabled(False)
+        else:
+            self._ui.lineEditName.setStyleSheet(DEFAULT_STYLE_SHEET)
+            self._ui.pushButtonSave.setEnabled(True)
+
     def _validate_marker_set(self):
         file_name = f"{self._ui.lineEditName.text()}.json"
         file_path = os.path.join(marker_maps_dir, file_name)
         if os.path.exists(file_path):
             QtWidgets.QMessageBox.warning(self, "Warning", f"Marker Set ({file_name}) already exists.")
+            return
+
+        self._marker_mapping = {}
+        for marker in markers:
+            combo_box = getattr(self._ui, f"comboBox{marker}")
+            value = combo_box.currentText()
+            self._marker_mapping[marker] = value if value else None
+
+        missing = []
+        for marker in ["ASI", "KNE", "ANK", "MED", "HEE"]:
+            if self._marker_mapping[marker] is None:
+                missing.append(f"\"{marker}\"")
+
+        if self._marker_mapping["PSI"] is None and self._marker_mapping["SACR"] is None:
+            missing.append('either "PSI" or "SACR"')
+
+        if self._marker_mapping["KNEM"] is None and self._marker_mapping["KAX"] is None:
+            missing.append('either "KNEM" or "KAX"')
+
+        if missing:
+            QtWidgets.QMessageBox.warning(self, "Warning", "Marker set must define:\n- " + "\n- ".join(missing))
             return
 
         self.accept()
